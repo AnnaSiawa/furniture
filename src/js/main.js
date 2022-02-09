4b7f6043163d831b1977157682e6d1fada3b6b2f
a.kucheryavenkova <a.kucheryavenkova@m-games-ltd.com>
2022-02-09 17:30:57 +0300

    new ecran

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,41 @@
-const sum = require("./module/sum.js");
-// const sum = (a, b) => a + b;
-console.log(sum(1, 10));
-console.log(sum(12, 5));
+import Swiper, { Navigation, Pagination } from 'swiper';
+Swiper.use([Navigation, Pagination]);
+
+const swiper = new Swiper('.product-swiper', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    watchOverflow: true,
+    loop: true,
+    slidesPerGroup: 1,
+    breakpoints: {
+        1620: {
+            slidesPerView: 5,
+            spaceBetween: 60,
+        },
+        1380: {
+            slidesPerView: 4,
+            spaceBetween: 60,
+        },
+        991: {
+            slidesPerView: 3,
+            spaceBetween: 60,
+        },
+        768: {
+            centeredSlides: false,
+            slidesPerView: 2,
+            spaceBetween: 60,
+        },
+        300: {
+            centeredSlides: true,
+            slidesPerView: 1,
+            spaceBetween: 60,
+        }
+    }
+});
+
+// const sum = require("./module/sum.js");
+// // const sum = (a, b) => a + b;
+// console.log(sum(1, 10));
+// console.log(sum(12, 5));
